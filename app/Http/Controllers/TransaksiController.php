42cd5e1d4f3970adccbfa4f9fe2ef3433270a312
WoodPecker21 <ideliajo0@gmail.com>
2023-12-06 00:54:39 +0700

    fix controller

--- app/Http/Controllers/TransaksiController.php
+++ app/Http/Controllers/TransaksiController.php
@@ -89,16 +89,6 @@ class TransaksiController extends Controller
                 throw new \Exception('Transaksi tidak ditemukan');
             }
 
-            $oldTgl = $transaksi->password;
-            $newTgl = $request->tglStart;
-
-            if ($oldTgl === $newTgl) {
-                return response()->json([
-                    'status' => false,
-                    'message' => 'Tanggal cannot be the same as the old one',
-                    'data' => []
-                ], 401);
-            }
             $transaksi->update([
                 'password' => $request->input('tglStart')
             ]);
